915a0a1671fa81cfbeeb4e386cd154981ca10460
ZhangP12 <paul.zheyang.zhang@gmail.com>
2017-11-30 11:00:03 -0800

    Imported files for 1-BasicPrinting & 2-Variables
    Removed earlier repository (that wasn't syncing files from Eclipse
    properly) so I created this repository and imported all files back on.

---
+++ 1-BasicPrinting/src/YourInitials.java
@@ -0,0 +1,17 @@
+
+public class YourInitials {
+
+	public static void main(String[] args) {
+		
+		System.out.println("PPPP   ZZZZZ");
+		System.out.println("P   P      Z");
+		System.out.println("P   P     Z ");
+		System.out.println("PPPP     Z  ");
+		System.out.println("P       Z   ");
+		System.out.println("P      Z    ");
+		System.out.println("P      ZZZZZ");
+		
+		
+	}
+	
+}
---
+++ 2-Variables/src/YourSchedule.java
@@ -0,0 +1,30 @@
+
+public class YourSchedule {
+
+	public static void main(String[] args) {
+		
+		String class1, class2, class3, class4;
+		String teacher1, teacher2, teacher3, teacher4;
+		
+		class1 = "Math 2";
+		class2 = "ECS";
+		class3 = "Biology";
+		class4 = "ISPE";
+		
+		teacher1 = "Shea";
+		teacher2 = "Mauro";
+		teacher3 = "Balch";
+		teacher4 = "Thomson";
+		
+		System.out.println("------------------------------------------------------------"
+				+ "+");
+		System.out.println("|Per 1 |     " + class1 + "     |     " + teacher1 + "     |");
+		System.out.println("|Per 2 |     " + class2 + "     |     " + teacher2 + "     |");
+		System.out.println("|Per 3 |     " + class3 + "     |     " + teacher3 + "     |");
+		System.out.println("|Per 4 |     " + class4 + "     |     " + teacher4 + "     |");
+		System.out.println("------------------------------------------------------------");
+		
+		
+	}
+	
+}
