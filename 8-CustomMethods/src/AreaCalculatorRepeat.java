e9eea3cc7b04bd5bbdd406280193bc49b27991e4
ZhangP12 <paul.zheyang.zhang@gmail.com>
2017-12-08 10:48:51 -0800

    Added some classes for 8-CustomMethods
    Still need to finish Part 3.

---
+++ 8-CustomMethods/src/AreaCalculatorRepeat.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AreaCalculatorRepeat
+{
+
+	public static void main(String[] args)
+	{
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println( "AREA OF SHAPES CALCULATOR\n" );
+		
+		while ( userInputShape != 5 )
+		{
+			
+			if userInputShape
+			
+		}
+		
+	}
+	
+}
